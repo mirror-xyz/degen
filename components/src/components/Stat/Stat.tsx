2e030e9a905bca7f00e7f67b9d9a9722aa6d2fe5
Jon-Kyle Mohr <contact@jon-kyle.com>
2021-11-02 18:15:04 -0700

    responsive heading

--- components/src/components/Stat/Stat.tsx
+++ components/src/components/Stat/Stat.tsx
@@ -14,7 +14,9 @@ export const Stat = ({ label, value, subValue, size = 'md' }: Props) => {
     <Stack space={size == 'md' ? '1' : '2'}>
       <Text variant="label">{label}</Text>
       {size === 'md' ? (
-        <Heading level="2">{value}</Heading>
+        <Heading level="2" responsive>
+          {value}
+        </Heading>
       ) : (
         <Text size="large" weight="semiBold">
           {subValue}
