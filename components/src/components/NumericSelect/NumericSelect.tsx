4621107e7ba9fe945b5356ed1540bb0407a23dd0
Nathaniel Grossman <nategrossman@mac.com>
2023-05-17 16:56:20 -0700

    make visual changes to numeric select

--- components/src/components/NumericSelect/NumericSelect.tsx
+++ components/src/components/NumericSelect/NumericSelect.tsx
@@ -35,13 +35,12 @@ export const NumericSelect = ({
   return (
     <Box
       alignItems="center"
-      backgroundColor="accentSecondary"
-      borderRadius="2xLarge"
+      backgroundColor="foregroundTertiary"
+      borderRadius="large"
       display="flex"
-      height="14"
+      height="10"
       justifyContent="center"
       position="relative"
-      width="full"
     >
       <Box
         alignItems="center"
@@ -54,9 +53,14 @@ export const NumericSelect = ({
         position="absolute"
         onClick={decrement}
       >
-        <IconMinus color={isMin ? 'accentSecondary' : 'accent'} size="5" />
+        <IconMinus
+          color={isMin ? 'textTertiary' : 'textSecondary'}
+          size="3.5"
+        />
       </Box>
-      <Text color="accent">{count}</Text>
+      <Text color="text" size="small" weight="semiBold">
+        {count}
+      </Text>
       <Box
         alignItems="center"
         display="flex"
@@ -67,7 +71,7 @@ export const NumericSelect = ({
         {showMaxButton && max && !isMax && (
           <Box
             as="button"
-            backgroundColor="accentSecondary"
+            backgroundColor="foregroundTertiary"
             borderRadius="medium"
             cursor={isMax ? 'not-allowed' : 'pointer'}
             disabled={isMax}
@@ -75,7 +79,7 @@ export const NumericSelect = ({
             padding="1"
             onClick={setMax}
           >
-            <Text color="accent" size="label">
+            <Text color="textSecondary" size="label">
               Max
             </Text>
           </Box>
@@ -87,7 +91,10 @@ export const NumericSelect = ({
           padding="2.5"
           onClick={increment}
         >
-          <IconPlus color={isMax ? 'accentSecondary' : 'accent'} size="5" />
+          <IconPlus
+            color={isMax ? 'textTertiary' : 'textSecondary'}
+            size="3.5"
+          />
         </Box>
       </Box>
     </Box>
