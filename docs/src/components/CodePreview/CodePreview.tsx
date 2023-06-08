0fe57e9582e4ed1ecee89d10eda60bd79ae59968
Cash Baller <cashd@users.noreply.github.com>
2023-06-08 17:17:02 -0500

    fix missing avatars

--- docs/src/components/CodePreview/CodePreview.tsx
+++ docs/src/components/CodePreview/CodePreview.tsx
@@ -27,6 +27,21 @@ const initialState = {
   expand: false,
 }
 
+export const avatars = {
+  noun2:
+    'https://images.mirror-media.xyz/publication-images/fHy5DSiGF6RZg8MYo3y_P.png',
+  noun3:
+    'https://images.mirror-media.xyz/publication-images/qmqVLl7GHPj4xfXRj21H-.png',
+  noun11:
+    'https://images.mirror-media.xyz/publication-images/9yZxF2aqRVvtb6xvYDOjs.png',
+  noun17:
+    'https://images.mirror-media.xyz/publication-images/DHNSNObmLOGtxjcluR7w2.png',
+  noun97:
+    'https://images.mirror-media.xyz/publication-images/H-zIoEYWk4SpFkljJiwB9.png',
+  noun102:
+    'https://images.mirror-media.xyz/publication-images/ncbsh8OPR_VdkRhGSljJD.png',
+}
+
 export const CodePreview = ({
   backgroundColor = 'background',
   code,
@@ -52,6 +67,7 @@ export const CodePreview = ({
         vars,
         NextImage,
         NextLink,
+        avatars,
       }}
       theme={theme}
       transformCode={(code) => '/** @jsx mdx */' + code}
