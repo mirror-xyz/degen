fd3d561827e4ea8a1f0c7e6a67c227aa7755fea9
Cash Baller <cashd@users.noreply.github.com>
2023-06-08 17:08:19 -0500

    remove playroom package

--- playroom/src/themes.ts
+++
@@ -1,2 +0,0 @@
-export const light = 'light'
-export const dark = 'dark'
--- playroom/src/types.ts
+++
@@ -1,9 +0,0 @@
-import { Snippets as PlayroomSnippets } from 'playroom'
-import { Optional } from 'utility-types'
-
-export type Snippet = Omit<
-  Optional<PlayroomSnippets[number], 'group'>,
-  'code'
-> & {
-  code: React.ReactElement
-}
