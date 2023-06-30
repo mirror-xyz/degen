3dcafeb8124fbd734ff116fcdce0cc7c7ac302bd
Nathaniel Grossman <nategrossman@mac.com>
2023-06-30 14:09:19 -0700

    support ens avatar without wagmi

--- components/src/components/Avatar/Avatar.tsx
+++ components/src/components/Avatar/Avatar.tsx
@@ -3,6 +3,11 @@ import * as React from 'react'
 import { Box, BoxProps } from '../Box'
 import * as styles from './styles.css'
 
+export type EnsAvatarHook = (
+  address: `0x${string}` | undefined,
+  callback: (uri: string | undefined) => void,
+) => void
+
 export type Props = {
   as?: 'img' | React.ComponentType
   label: string
@@ -11,6 +16,7 @@ export type Props = {
   noBorder?: boolean
   size?: BoxProps['height']
   src?: string
+  ensAvatarHook?: EnsAvatarHook
 } & styles.Variants
 
 export function getAvatarGradient(address = '0') {
@@ -44,8 +50,12 @@ export const Avatar = ({
   shape = 'circle',
   size = '12',
   src,
+  ensAvatarHook: useEnsAvatar = () => undefined,
 }: Props) => {
+  const [ensAvatar, setEnsAvatar] = React.useState<string>()
   const [error, setError] = React.useState(false)
+
+  useEnsAvatar(address as `0x${string}`, setEnsAvatar)
 
   const onError = React.useMemo(() => {
     if (as !== 'img' || error) {
@@ -55,7 +65,9 @@ export const Avatar = ({
     return () => setError(true)
   }, [as, error])
 
-  const showPlaceholder = placeholder || error || !src
+  const srcOrEns = src ?? ensAvatar ?? undefined
+
+  const showPlaceholder = placeholder || error || !srcOrEns
 
   return (
     <Box
@@ -82,7 +94,7 @@ export const Avatar = ({
             as={as}
             height="full"
             objectFit="cover"
-            src={src}
+            src={srcOrEns}
             width="full"
             {...{
               decoding: 'async',
